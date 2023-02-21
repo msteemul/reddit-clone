ad41c4180f7dc9f02e229d339d1143fb3507a2f9
msteemul <113412804+msteemul@users.noreply.github.com>
2023-02-22 01:16:47 +0400

    assets

--- my-reddit-clone/src/pages/components/Navbar/Navbar.tsx
+++ my-reddit-clone/src/pages/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
+import { Image } from '@chakra-ui/react';
 
 // type NavbarProps = {
     
@@ -9,7 +10,13 @@ const Navbar:React.FC = () => {
     
     return (
         <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex>Logo</Flex>
+            <Flex align="center">
+                <Image src='/images/redditFace.svg' height='30px'/>
+                <Image src='/images/redditText.svg' height='46px' display={{ base: "none", md: 'unset'}}/>
+            </Flex>
+            {/* <Directory /> */}
+            {/* <SearchInput /> */}
+            {/* <RightContent /> */}
 
         </Flex>
     )
